0935be6a769b3fb7e0337ff3c60a496b6236e9ac
Tariqkhanmohmand <67182268+Tariqkhanmohmand@users.noreply.github.com>
2020-08-06 15:44:15 +0500

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,9 +0,0 @@
-const helloSpeaker = {
-    speakWord: 'Hello',
-};
-//This says Hello
-(function() {
-    helloSpeaker.speak = function(userName) {
-        return (helloSpeaker.speakWord + " " + userName);
-    }
-}());
